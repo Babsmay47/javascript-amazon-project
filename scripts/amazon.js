aa9b8d2d2d556d0c6945f0699043ac58600bde15
Babsmay47 <babsmay47@gmail.com>
2025-05-18 18:13:26 +0100

    Make the Selector Active and get its value

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -73,11 +73,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     if(matchingItem){
-      matchingItem.quantity += 1;
+      matchingItem.quantity += Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
     } else{
       cart.push({
         productId: productId,
-        quantity: 1
+        quantity: Number(document.querySelector(`.js-quantity-selector-${productId}`).value)
       });
     }
 
@@ -88,5 +88,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    // Number(document.querySelector(`.js-quantity-selector-${productId}`).value)
   });
 });
